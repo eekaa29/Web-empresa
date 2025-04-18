c05ba41183a216ad741be9eec08080926bddfb96
Ekaitz Martin <ekamartinc2003@example.com>
2025-04-18 17:41:23 +0200

    readonly_fields for users that are part of --Personal-- group

--- social/admin.py
+++ social/admin.py
@@ -4,4 +4,12 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ("created", "updated")
 
+    def get_readonly_fields(self, request, obj = None):#Funcion para añadir campos al apartado de readonly_fields
+        if request.user.groups.filter(name="Personal").exists():#Comprobar en tiempo de ejecución si el usuario forma parte del grupo "Personal"
+            return ("created", "updated", "key", "name")
+        else:
+            return ("created", "updated")
+
+        
+
 admin.site.register(Link, LinkAdmin)
